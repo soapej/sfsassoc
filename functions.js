349e6927259705974c8b7b18139aa5306bc60f2a
soapej <jonathansoape@gmail.com>
2020-06-07 15:47:16 -0500

    initial

---
+++ functions.js
@@ -0,0 +1,33 @@
+$(function(){
+
+	var slides = [
+		"Contingency Fee - If I am not successful, there is no charge",
+		"Participate in my fee sharing arrangement",
+		"I will aggressively negotiate the most favorable settlement terms for you"
+	];
+
+	
+	function blueNav(){
+		$('nav').css('background-color', '#d1edfa');
+	}
+	function clearNav(){
+		$('nav').css('background-color', 'transparent');
+	}
+
+
+	$('.articles a').mouseenter(function(){
+		$(this).find('.text').addClass('show');
+	});
+	$('.articles a').mouseleave(function(){
+		$(this).find('.text').removeClass('show');
+	});
+	
+	$(window).scroll(function(){
+		if($(window).scrollTop() > 90) {
+			blueNav();
+		} else {
+			clearNav();
+		}
+	});
+
+});
